69767aedc5bfaa069ed17e5dc349c7b1bc15005d
ian <developer.kss@gmail.com>
2019-05-22 08:56:27 +0900

    TabNavigation style

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -3,11 +3,19 @@ import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import MovieScreen from "../screens/Movies.js";
 import TVScreen from "../screens/TV.js";
 import SearchScreen from "../screens/Search.js";
+import BG_COLOR from "../constants/Colors.js";
 
 const TabNavation = createBottomTabNavigator({
-    MovieScreen,
-    TVScreen,
-    SearchScreen
+    Movies: MovieScreen,
+    TV: TVScreen,
+    Search: SearchScreen
+}, {
+    tabBarOptions: {
+        showLabel:false,
+        style:{
+            backgroundColor:BG_COLOR
+        }
+    }
 });
 
 export default createAppContainer(TabNavation);
