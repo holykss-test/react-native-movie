6746e6e5edc12de2d00b81a9aa43a95c8eb6e8a1
ian <developer.kss@gmail.com>
2019-05-22 10:22:04 +0900

    app bar icon

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,37 @@
+import React from "react";
+import {Platform} from "react-native";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 
 import MovieScreen from "../screens/Movies.js";
 import TVScreen from "../screens/TV.js";
 import SearchScreen from "../screens/Search.js";
 import BG_COLOR from "../constants/Colors.js";
+import TabBarIcon from "../components/TabBarIcon.js";
 
 const TabNavation = createBottomTabNavigator({
-    Movies: MovieScreen,
-    TV: TVScreen,
-    Search: SearchScreen
+    Movies: { screen: MovieScreen, 
+    navigationOptions:{
+        tabBarIcon:({focused}) => (
+        <TabBarIcon focused={focused} name={(Platform.OS == "ios" ? "ios" : "md") + "-film"} />
+    )
+    }},
+    TV: { screen: TVScreen, 
+    navigationOptions:{
+        tabBarIcon:({focused}) => (
+        <TabBarIcon focused={focused} name={(Platform.OS == "ios" ? "ios" : "md") + "-tv"} />
+    )
+    }},
+    Search: { screen: SearchScreen, 
+    navigationOptions:{
+        tabBarIcon:({focused}) => (
+        <TabBarIcon focused={focused} name={(Platform.OS == "ios" ? "ios" : "md") + "-search"} />
+    )
+    }}
 }, {
     tabBarOptions: {
-        showLabel:false,
+        showLabel:true,
         style:{
-            backgroundColor:BG_COLOR
+            backgroundColor: BG_COLOR
         }
     }
 });
