aadb8b82fbcd29612e8a47457aa787e715aa921f
ian <developer.kss@gmail.com>
2019-05-22 10:27:06 +0900

    {BG_COLOR}

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -5,7 +5,7 @@ import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import MovieScreen from "../screens/Movies.js";
 import TVScreen from "../screens/TV.js";
 import SearchScreen from "../screens/Search.js";
-import BG_COLOR from "../constants/Colors.js";
+import {BG_COLOR} from "../constants/Colors.js";
 import TabBarIcon from "../components/TabBarIcon.js";
 
 const TabNavation = createBottomTabNavigator({
